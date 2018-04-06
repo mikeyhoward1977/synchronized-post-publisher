82f802b3de5dfaa072fea60b282d3f37df267a81
Mike Howard <mike@mikeandniki.co.uk>
2018-04-06 10:17:09 +0100

    Added group filter to enabled post lists

--- includes/admin/post-spp-groups.php
+++ includes/admin/post-spp-groups.php
@@ -84,3 +84,63 @@ function wp_spp_output_group_post_columns( $column_name, $post_id ) {
 
 } // wp_spp_output_group_post_columns
 add_action( 'manage_wp_spp_group_posts_custom_column' , 'wp_spp_output_group_post_columns', 10, 2 );
+
+/**
+ * Adds the SPP Group filter dropdown to enabled post types.
+ *
+ * @since   1.0
+ * @return  void
+ */
+function wp_spp_add_group_filters() {
+    global $typenow;
+
+    if ( in_array( $typenow, wp_spp_group_post_types() ) )  {
+        $group_options = array();
+        $groups        = wp_spp_get_post_sync_groups();
+
+        if ( ! empty( $groups ) )   {
+            foreach( $groups as $group )    {
+
+                $posts_in_group = wp_spp_get_posts_in_sync_group( $group->ID );
+                if ( empty( $posts_in_group ) ) {
+                    continue;
+                }
+
+                foreach( $posts_in_group as $post ) {
+                    $group_options[ $group->ID ] = esc_html( $group->post_title ) . ' (' . count( $posts_in_group ) . ')';
+                }
+            }
+        }
+
+        if ( ! empty( $group_options ) )    {
+            echo "<select name='spp_group' id='spp_group' class='postform'>";
+
+                echo "<option value=''>" . __( 'Show all SPP groups', 'kb-support' ) . "</option>";
+
+                foreach( $group_options as $option_id => $option_value )  {
+                    $selected = isset( $_GET['spp_group'] ) && $_GET['spp_group'] == $option_id ? ' selected="selected"' : '';
+					echo '<option value="' . esc_attr( $option_id ) . '"' . $selected . '>' . $option_value . '</option>';
+                }
+
+            echo "</select>";
+        }
+    }
+} // wp_spp_add_group_filters
+add_action( 'restrict_manage_posts', 'wp_spp_add_group_filters', 100 );
+
+/**
+ * Filter tickets by SPP Group.
+ *
+ * @since	1.0
+ * @return	void
+ */
+function wp_spp_filter_posts_by_group( $query )	{
+	if ( ! is_admin() || ! in_array( $query->get( 'post_type' ), wp_spp_group_post_types() )  || ! isset( $_GET['spp_group'] ) )	{
+		return;
+	}
+
+	$query->set( 'meta_key', '_wp_spp_sync_group' );
+	$query->set( 'meta_value', $_GET['spp_group'] );
+	$query->set( 'meta_type', 'NUMERIC' );
+} // wp_spp_filter_posts_by_group
+add_action( 'pre_get_posts', 'wp_spp_filter_posts_by_group' );
